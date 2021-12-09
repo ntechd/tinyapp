431f4488c6be6436195b1b5bf95f6458c05d43ba
Najma Duale <najmaduale1@gmail.com>
2021-12-09 04:39:59 +0000

    user registation form

--- express_server.js
+++ express_server.js
@@ -34,6 +34,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"]
+  };
+  res.render("user_registration", templateVars);
+});
+
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
